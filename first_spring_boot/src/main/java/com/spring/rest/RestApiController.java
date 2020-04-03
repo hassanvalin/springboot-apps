c61ad7bc4ef658541460d990d2a526cf027233b7
hassanvalin <hassanvalin@gmail.com>
2020-04-02 23:13:50 -0700

    Update RestApiController.java

--- first_spring_boot/src/main/java/com/spring/rest/RestApiController.java
+++ first_spring_boot/src/main/java/com/spring/rest/RestApiController.java
@@ -3,7 +3,7 @@ package com.spring.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import util.Date;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/app")
