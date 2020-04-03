efb5d64d0a812f0a6c69eec8fc5dd698bacd240d
hassanvalin <hassanvalin@gmail.com>
2020-04-02 23:06:59 -0700

    Update RestApiController.java

--- first_spring_boot/src/main/java/com/spring/rest/RestApiController.java
+++ first_spring_boot/src/main/java/com/spring/rest/RestApiController.java
@@ -3,12 +3,14 @@ package com.spring.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import util.Date;
 
 @RestController
 @RequestMapping("/app")
 public class RestApiController {
 	@GetMapping("/index")
 	public String index() {
+		System.out.println("Welcome: " + new Date())
 		return "Welcome to the home page!";
 	}
 }
